5ce193898a81f6b3c5daf0238e605d621776875d
Oluwatunmise <oluwatunmiseadenuga@crenettechlabs.com>
2021-02-03 11:16:21 +0100

    chore: ui for reset password

--- app-admin/pages/auth/reset-password.vue
+++ app-admin/pages/auth/reset-password.vue
@@ -13,34 +13,31 @@
       <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
         <div class="mb-8">
           <h2 class="mt-6 text-2xl font-bold text-gray-800">
-            Reset Your Password
+            Enter Your New Password
           </h2>
           <p class="font-normal text-base text-gray-600">
-            A password reset link will be sent to the email associated with your account.
+            Change your password by entering a new one.
           </p>
         </div>
         <form class="space-y-6">
           <!-- email field -->
           <div>
-            <selfcare-input-label label="Email" />
+            <selfcare-input-label label="Password" inputFor="password" />
             <div class="mt-1">
-              <selfcare-input type="email" id="email" />
+              <selfcare-input type="password" id="password"/>
+            </div>
+          </div>
+
+          <div>
+            <selfcare-input-label label="confirm Password" inputFor="confirm-password" />
+            <div class="mt-1">
+              <selfcare-input type="password" id="confirm-password"/>
             </div>
           </div>
 
           <!-- submit button -->
           <div>
-            <selfcare-button button-label="Send Me a link" />
-          </div>
-
-          <div class="flex items-center mt-3">
-            <p class="font-medium text-gray-700 hover:text-gray-500 mb-0 pr-1">Remember your password?</p>
-            <nuxt-link 
-              :to="{path: '/'}"
-              class="font-medium text-blue-700 hover:text-blue-500 hover:underline"
-            >
-              Login
-            </nuxt-link>
+            <selfcare-button button-label="Reset Password" />
           </div>
         </form>
       </div>
