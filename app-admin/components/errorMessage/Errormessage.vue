8aef6f1536fefc66557777c7bdf3ab9db33a4c78
Oluwatunmise <oluwatunmiseadenuga@crenettechlabs.com>
2021-03-07 00:54:40 +0100

    feat(component): created component to render error message

---
+++ app-admin/components/errorMessage/Errormessage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex items-center text-red-500 text-xs mt-2">
+    <ErrorInfoIcon /> 
+    <span>{{message}}</span>
+  </div>
+</template>
+<script>
+import ErrorInfoIcon from "@/components/vectors/ErrorInfo"
+export default {
+  components: {
+    ErrorInfoIcon
+  },
+  props: {
+    message: {
+      type: String,
+      default: "",
+      required: true
+    }
+  }
+}
+</script>
