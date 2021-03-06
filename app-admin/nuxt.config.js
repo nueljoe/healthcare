4e55ae6fdd95577a8881cfb369cd12ae4d5bf485
Oluwatunmise <oluwatunmiseadenuga@crenettechlabs.com>
2021-03-07 00:52:25 +0100

    chore(setup): renamed app name from selfcare to selfcare |  admin

--- app-admin/nuxt.config.js
+++ app-admin/nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Selfcare',
+    title: 'Selfcare | Admin',
     htmlAttrs: {
       lang: 'en',
     },
