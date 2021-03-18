24d49bdb80ef497d1f58e95d4068524c4129e4a9
Oluwatunmise <oluwatunmiseadenuga@crenettechlabs.com>
2021-03-18 20:57:42 +0100

    chore: extended tailwind utility classes

--- app-admin/tailwind.config.js
+++ app-admin/tailwind.config.js
@@ -1,7 +1,21 @@
-// module.exports = {
-//   theme: {
-//     colors: {
-//      primary: "#E77917"
-//     }
-//   }
-// }
+const { colors } = require('tailwindcss/defaultTheme')
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        green: {
+          50: "#ECFDF5",
+          ...colors.green
+        },
+        red: {
+          50: "#FEF2F2",
+          ...colors.red
+        },
+        blue: {
+          50: "#EFF6FF",
+          ...colors.blue
+        }
+      }
+    }
+  }
+}
