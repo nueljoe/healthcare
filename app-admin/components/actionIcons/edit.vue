322f07ec8cba4c5f2cac9c837c54982cd69ecd79
Oluwatunmise <oluwatunmiseadenuga@crenettechlabs.com>
2021-03-29 10:09:09 +0100

    chore: added action button to edit a table data

--- app-admin/components/actionIcons/edit.vue
+++ app-admin/components/actionIcons/edit.vue
@@ -1,5 +1,6 @@
 <template>
-  <button 
+  <button
+    @click="$emit('editButtonClicked')"
     type="button" 
     class="relative inline-flex items-center px-2 py-2 border border-cool-gray-300 text-sm leading-5 font-medium rounded-md text-cool-gray-700 bg-white hover:text-cool-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-cool-gray-100 active:text-cool-gray-700 transition ease-in-out duration-150"
   >
