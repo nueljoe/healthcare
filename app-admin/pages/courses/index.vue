00f6cc59409fb90473ab97b25907243e8b8ef207
Oluwatunmise <oluwatunmiseadenuga@crenettechlabs.com>
2021-03-29 10:30:53 +0100

    chore: created new page for courses

--- app-admin/pages/courses/index.vue
+++ app-admin/pages/courses/index.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="mt-8 max-w-auto mx-auto px-4 sm:px-6 lg:px-8">
       <div class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-1 lg:grid-cols-8">
-        <!-- categories table -->
+        <!-- courses table -->
         <div class="col-span-5"> 
           <div class="flex justify-end mb-2">
             <span class="relative z-0 inline-flex shadow-sm rounded-md">
@@ -15,7 +15,7 @@
           <courses-table />
         </div>
 
-          <!-- add new category form -->
+          <!-- add new course form -->
           <div class="col-span-3"> 
             <div class="shadow sm:rounded-lg overflow-hidden">
             <div class="px-6 py-3 bg-gray-200 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider border-b border-gray-300">
@@ -32,8 +32,8 @@
 </template>
 
 <script>
-  import CoursesTable from '@/components/tables/courses'
-   import CreateCourseForm from '@/components/forms/course'
+  import CoursesTable from '@/components/tables/courses/courses'
+  import CreateCourseForm from '@/components/forms/course'
   export default {
     data() {
       return {
