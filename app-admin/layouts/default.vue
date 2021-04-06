f9c2c3cadfaf0c1369b8e2a67d482d26fe18ffea
Oluwatunmise <oluwatunmiseadenuga@crenettechlabs.com>
2021-04-06 19:26:41 +0100

    chore: updated default layout

--- app-admin/layouts/default.vue
+++ app-admin/layouts/default.vue
@@ -3,7 +3,7 @@
     <TheSideBar />
     <div class="flex-1 overflow-auto focus:outline-none" tabindex="0">
       <TheTopbar />
-      <main class="flex-1 relative h-auto pb-8 z-0 overflow-y-auto">
+      <main class="flex-1 z-20 relative h-auto pb-8 overflow-y-auto">
         <nuxt />
       </main>
     </div>
