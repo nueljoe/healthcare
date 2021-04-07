3c9e197c1f37bc28e1e6275e3efae6d5a8ebb438
Oluwatunmise <oluwatunmiseadenuga@crenettechlabs.com>
2021-04-07 11:42:40 +0100

    chore: created modal for creating and editing courses

---
+++ app-admin/components/modals/coursesModal.vue
@@ -0,0 +1,37 @@
+<template>
+  <modal 
+    :name="modalName" 
+    :clickToClose="false"
+    height="auto"
+    :scrollable="true"
+    width="650"
+    classes="overflow-visible"
+  >
+    <div class="p-5">
+    <close-modal @closeModal="$modal.hide(`${modalName}`)" />
+      <h4 class="modal-title">
+        {{ actionType === 'edit' ? 'Edit' : 'Create' }} Course
+      </h4>
+      <course-form />
+    </div>
+  </modal>
+</template>
+
+<script>
+  const CloseModal = () => import('@/components/button/CloseModalButton');
+  import CourseForm from '@/components/forms/course';
+  export default {
+    props: {
+      actionType: {
+        type: String
+      },
+      modalName: {
+        type: String
+      }
+    },
+    components: {
+      CourseForm,
+      CloseModal
+    }
+  }
+</script>
