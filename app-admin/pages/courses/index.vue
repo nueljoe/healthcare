42f9d055228976da314f3202088d13ccb00c3629
Oluwatunmise <oluwatunmiseadenuga@crenettechlabs.com>
2021-04-07 11:46:08 +0100

    chore: added action buttons to create and export table data

--- app-admin/pages/courses/index.vue
+++ app-admin/pages/courses/index.vue
@@ -3,42 +3,39 @@
     <div class="mt-8 max-w-auto mx-auto px-4 sm:px-6 lg:px-8">
       <div class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-1 lg:grid-cols-8">
         <!-- courses table -->
-        <div class="col-span-5"> 
+        <div class="col-span-8"> 
           <div class="flex justify-end mb-2">
-            <span class="relative z-0 inline-flex shadow-sm rounded-md">
+            <span class="relative z-0 inline-flex shadow-sm">
+              <selfcare-button 
+                button-label="Create course" 
+                button-class="selfare-button__light"
+                :rounded="false"
+                @click="$modal.show('create-course-modal')"
+              />
+            </span>
+            <span class="relative z-0 inline-flex shadow-sm">
               <selfcare-button 
                 button-label="Export" 
-                button-class="selfare-button__light" 
+                button-class="selfare-button__light rounded-none"
+                :rounded="false"
               />
             </span>
           </div>
           <courses-table />
         </div>
-
-        <!-- add new course form -->
-        <div class="col-span-3"> 
-          <div class="shadow sm:rounded-lg overflow-hidden">
-            <div 
-              class="
-                px-6 py-3 bg-gray-200 text-left text-xs leading-4 font-medium 
-              text-gray-500 uppercase tracking-wider border-b border-gray-300
-              "
-            >
-              Create course
-            </div>
-            <div class="bg-white w-full px-6 py-6">
-              <create-course-form />
-            </div>
-          </div>
-        </div>
       </div>
     </div>
+
+    <!-- modal to create course  -->
+    <create-course-modal 
+      modalName="create-course-modal"
+    />
   </div>
 </template>
 
 <script>
   import CoursesTable from '@/components/tables/courses/coursesTable'
-  import CreateCourseForm from '@/components/forms/course'
+  import CreateCourseModal from '@/components/modals/coursesModal'
   export default {
     data() {
       return {
@@ -47,7 +44,7 @@
     },
     components: {
       CoursesTable,
-      CreateCourseForm
+      CreateCourseModal
     }
   }
 
